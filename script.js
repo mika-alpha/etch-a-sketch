6b4135cec31d3fad9d65710733805da70aba3881
mika-alpha <yuukeikioku@gmail.com>
2022-05-19 19:22:22 -0500

    Add hover, fix createGrid

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let container = document.querySelector('.main');
 
+/*create columns, create rows, then add the columns to the rows,
+then add the rows to the grid */
+
+
 function createGrid(){
     for (let i = 0; i < 16; i++){
         let row = document.createElement('div');
@@ -13,4 +17,13 @@ function createGrid(){
     }
 }
 
+
 createGrid();
+
+let cells = Array.from(document.getElementsByClassName('column'));
+
+cells.forEach(cell => cell.addEventListener('mouseenter',hover));
+
+function hover(e){
+    e.target.classList.add('hovered');
+}
